aef1c3b3ea75431d460b346ceae6f0da2fa4adc6
yangzhi1992 <377163030@qq.com>
2017-06-17 12:32:00 +0800

    sit

--- yz/src/main/java/com/yz/designpattern/Strategy.java
+++ yz/src/main/java/com/yz/designpattern/Strategy.java
@@ -1,4 +1,5 @@
 package com.yz.designpattern;
 public interface Strategy {  
 	public int execute(int arg1, int arg2);
+	
 }
--- yz/src/main/java/com/yz/designpattern/StrategyMultiply.java
+++ yz/src/main/java/com/yz/designpattern/StrategyMultiply.java
@@ -2,5 +2,7 @@ package com.yz.designpattern;
 public class StrategyMultiply implements Strategy{
 	public int execute(int arg1, int arg2) {
 		return arg1 * arg2;
+		
+		
 	}
 }
