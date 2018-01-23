b356f064075cbd16e8d86327c6a5bdcf7d917601
yangzhi1992 <377163030@qq.com>
2018-01-23 16:33:34 +0800

    yangzhi

---
+++ yz/src/main/java/com/yz/utils/DefaultValueSetter.java
@@ -0,0 +1,34 @@
+package com.yz.utils;
+
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+
+public class DefaultValueSetter {
+	/**
+	 * 设置默认值
+	 * @param object
+	 */
+	public static <T> void setDefaultValueForObject(T object) {
+		Field[] fieldArray = object.getClass().getDeclaredFields();
+		for (Field field : fieldArray) {
+			field.setAccessible(true);
+			try {
+				if (null == field.get(object)) {
+					if (String.class.equals(field.getType())) {
+						field.set(object, "");
+					}
+					if (Integer.class.equals(field.getType())) {
+						field.set(object, new Integer(0));
+					}
+					if (BigDecimal.class.equals(field.getType())) {
+						field.set(object, new BigDecimal(0));
+					}
+					if (Double.class.equals(field.getType())) {
+						field.set(object, new Double(0));
+					}
+				}
+			} catch (Exception e) {
+			}
+		}
+	}
+}
